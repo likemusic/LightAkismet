883e0cb6e025ade2dc2e4866064e711c99b4d056
Valerij Ivashchenko <likemusic@yandex.ru>
2013-06-06 20:38:05 +0300

    fix some property/params name. add phpdoc comments to AlismetService class

--- AkismetService.php
+++ AkismetService.php
@@ -1,5 +1,5 @@
 <?php
-	
+
 	require_once('AkismetServiceSingleton.php');
 
 	/**
@@ -7,91 +7,163 @@
 	*/
 	class AkismetService
 	{
-		public $ApiKey;
-		public $BlogUrl;
-		public $HttpUserAgent;
-		public $DefaultCommentValues = array();
-		
 		/**
-		* Intarnal used singleton
+		* The API key being verified for use with the API
+		* @link http://akismet.com/development/api/#verify-key
+		* @var string
+		*/
+		public $Key;
+
+		/**
+		* Http-request header value of UserAgent field.
+		* Must be in format: "{Application Name}/{Version} | {Plugin Name}/{Version}"
+		* for excample "WordPress/3.1.1 | Akismet/2.5.3".
+		* @link http://akismet.com/development/api/#getting-started
+		* @var string
+		*/
+		public $HttpClientUserAgent;
+
+		/**
+		* The front page or home URL of the instance making the request.
+		* For a blog or wiki this would be the front page.
+		* Note: Must be a full URI, including http://.
+		* 
+		* @var string
+		*/
+		public $Blog;
+
+		/**
+		* Dafault comments propetries values
+		* 
+		* @var AkismetComment|array
+		*/
+		public $DefaultCommentValues = array();
+
+		/**
+		* Internal used singleton
 		* 
 		* @var AkismetServiceSingleton
 		*/
 		protected $AkismetSingleton;
 
+
 		const PARAM_NAME_BLOG = 'blog';
-		
+
 		/**
 		* Constructor
 		* 
-		* @param string $ApiKey
-		* @param string $HttpUserAgent
-		* @param string $BlogUrl
-		* @param string $DefaultCommentValues
-		* @param string $AutoVerifyKey
+		* @param string $Key The API key being verified for use with the API.
+		* @param string $HttpUserAgent Http-request header value of UserAgent field.
+		* @param string $Blog The front page or home URL of the instance making the request.
+		* @param array|AkismetComment $DefaultCommentValues Default comments propetries values
+		* @param string $AutoVerifyKey - Enable/Disable api key verification in constructor
 		* @return AkismetService
 		*/
-		public function __construct( $ApiKey=null, $HttpUserAgent=null, $BlogUrl=null, $DefaultCommentValues=null, $AutoVerifyKey = false ) {
-			$this->setDafaults( $DefaultCommentValues, $BlogUrl, $HttpUserAgent, $ApiKey );
+		public function __construct( $Key=null, $HttpUserAgent=null, $Blog=null, $DefaultCommentValues=null, $AutoVerifyKey = false ) {
+			$this->setDafaults( $DefaultCommentValues, $Blog, $HttpUserAgent, $Key );
 			$this->AkismetSingleton = AkismetServiceSingleton::getInstance();
-			
+
 			if( $AutoVerifyKey ) {
 				if( !$this->verifyKey() ) throw new Exception(self::EXCEPTION_MESSAGE_VERIFY_KEY_FAIL);
 			}
 		}
-		
+
 		/**
 		* Set dafult values for all Akismet variables
 		* 
-		* @param array|AkismetComment $CommentValues
-		* @param string $BlogUrl
-		* @param string $HttpUserAgent
-		* @param string $ApiKey
+		* @param array|AkismetComment $CommentValues Comments propetries values.
+		* @param string $Blog The front page or home URL of the instance making the request.
+		* @param string $HttpUserAgent Http-request header value of UserAgent field.
+		* @param string $Key The API key being verified for use with the API
 		*/
-		public function setDafaults( $CommentValues,  $BlogUrl=null, $HttpUserAgent=null, $ApiKey=null ) {
+		public function setDafaults( $CommentValues,  $Blog=null, $HttpUserAgent=null, $Key=null ) {
 			if( $CommentValues !== null ) $this->DefaultCommentValues = $CommentValues;
-			if( $BlogUrl !== null ) $this->BlogUrl = $BlogUrl;
-			if( $this->BlogUrl === null ) $this->BlogUrl = $DefaultCommentValues[self::PARAM_NAME_BLOG];
-			
-			if( $HttpUserAgent !== null ) $this->HttpUserAgent = $HttpUserAgent;
-			if( $ApiKey !== null ) $this->ApiKey = $ApiKey;
+			if( $Blog !== null ) $this->Blog = $Blog;
+			if( $this->Blog === null ) $this->Blog = $DefaultCommentValues[self::PARAM_NAME_BLOG];
+
+			if( $HttpUserAgent !== null ) $this->HttpClientUserAgent = $HttpUserAgent;
+			if( $Key !== null ) $this->Key = $Key;
 		}
-		
-		public function verifyKey( $BlogUrl = null, $HttpUserAgent=null, $ApiKey = null ) {
-            $this->setParamsVars( $BlogUrl, $HttpUserAgent, $ApiKey );
-			$Result = self::verifyKey( $ApiKey, $HttpUserAgent, $BlogUrl );
+
+		/**
+		* Verify Api key
+		* @link http://akismet.com/development/api/#verify-key
+		* 
+		* @param string $Blog The front page or home URL of the instance making the request.
+		* @param string $HttpUserAgent Http-request header value of UserAgent field.
+		* @param string $Key The API key being verified for use with the API.
+		*/
+		public function verifyKey( $Blog = null, $HttpUserAgent=null, $Key = null ) {
+			$this->setParamsVars( $Blog, $HttpUserAgent, $Key );
+			$Result = self::verifyKey( $Key, $HttpUserAgent, $Blog );
 			return $Result;
 		}
 
-		protected function setParamsVars( &$BlogUrl, &$HttpUserAgent, &$ApiKey ) {
-			
-			if( $BlogUrl===null ) {
-				$BlogUrl = $this->BlogUrl;
+		/**
+		* Check comment.
+		* @link http://akismet.com/development/api/#comment-check
+		* 
+		* @param array|AkismetComment $CommentValues  Comments propetries values.
+		* @param string $Blog The front page or home URL of the instance making the request.
+		* @param string $HttpUserAgent Http-request header value of UserAgent field.
+		* @param string $Key The API key being verified for use with the API
+		*/
+		public function checkComment ( $CommentValues, $Blog=null, $HttpUserAgent=null, $Key=null ) {
+			$this->setParamsVars( $Blog, $HttpUserAgent, $Key );
+			$Result = $this->AkismetSingleton->checkComment( $Key, $HttpUserAgent, $CommentValues, $Blog );
+			return $Result;
+		}
+
+
+		/**
+		* Submit spam.
+		* @link http://akismet.com/development/api/#submit-spam
+		* 
+		* @param array|AkismetComment $CommentValues  Comments propetries values.
+		* @param string $Blog The front page or home URL of the instance making the request.
+		* @param string $HttpUserAgent Http-request header value of UserAgent field.
+		* @param string $Key The API key being verified for use with the API
+		*/
+		public function submitSpam( $CommentValues, $Blog=null, $HttpUserAgent=null, $Key=null ) {
+			$this->setParamsVars( $Blog, $HttpUserAgent, $Key );
+			return $this->AkismetSingleton->submitSpam($Key, $HttpUserAgent, $CommentValues, $Blog);
+		}
+
+		/**
+		* Submit Ham
+		* @link http://akismet.com/development/api/#submit-ham
+		* 
+		* @param array|AkismetComment $CommentValues  Comments propetries values.
+		* @param string $Blog The front page or home URL of the instance making the request.
+		* @param string $HttpUserAgent Http-request header value of UserAgent field.
+		* @param string $Key The API key being verified for use with the API
+		*/
+		public function submitHam( $CommentValues, $Blog=null, $HttpUserAgent=null, $Key=null ) {
+			$this->setParamsVars( $Blog, $HttpUserAgent, $Key );
+			return $this->AkismetSingleton->submitHam( $CommentValues, $Blog, $HttpUserAgent, $Key );
+		}
+
+
+		/**
+		* Set result values for variables that will be passed to singleton methods
+		* 
+		* @param string $Blog
+		* @param string $HttpUserAgent
+		* @param string $Key
+		*/
+		protected function setParamsVars( &$Blog, &$HttpUserAgent, &$Key ) {
+
+			if( $Blog===null ) {
+				$Blog = $this->Blog;
 			}
 
 			if( $HttpUserAgent===null ) {
-				$HttpUserAgent = $this->HttpUserAgent;	
+				$HttpUserAgent = $this->HttpClientUserAgent;	
 			}
 
-			if( $ApiKey===null ) {
-				$ApiKey = $this->ApiKey;	
+			if( $Key===null ) {
+				$Key = $this->Key;	
 			}
-		}
-		
-		public function submitSpam( $CommentValues, $BlogUrl=null, $HttpUserAgent=null, $ApiKey=null ) {
-            $this->setParamsVars( $BlogUrl, $HttpUserAgent, $ApiKey );
-			return $this->AkismetSingleton->submitSpam($ApiKey, $HttpUserAgent, $CommentValues, $BlogUrl);
-		}
-
-		public function submitHam( $CommentValues, $BlogUrl=null, $HttpUserAgent=null, $ApiKey=null ) {
-            $this->setParamsVars( $BlogUrl, $HttpUserAgent, $ApiKey );
-			return $this->AkismetSingleton->submitHam( $CommentValues, $BlogUrl, $HttpUserAgent, $ApiKey );
-		}
-
-		public function checkComment ( $CommentValues, $BlogUrl=null, $HttpUserAgent=null, $ApiKey=null ) {
-            $this->setParamsVars( $BlogUrl, $HttpUserAgent, $ApiKey );
-			$Result = $this->AkismetSingleton->checkComment( $ApiKey, $HttpUserAgent, $CommentValues, $BlogUrl );
-			return $Result;
 		}
 	}
 
