17a4178f2ea75f3129a75d3c5c4598d6daaac017
Valerij Ivashchenko <likemusic@yandex.ru>
2013-06-06 18:15:50 +0300

    Add setDefaults() method to AkismetService class. Refactor setting prameters in method calls

--- AkismetService.php
+++ AkismetService.php
@@ -10,15 +10,29 @@
 		public $ApiKey;
 		public $BlogUrl;
 		public $HttpUserAgent;
-		public $DefaultValues = array();
+		public $DefaultCommentValues = array();
 		
+		/**
+		* Intarnal used singleton
+		* 
+		* @var AkismetServiceSingleton
+		*/
 		protected $AkismetSingleton;
 
-
-		public function __construct( $ApiKey=null, $HttpUserAgent=null, $BlogUrl=null, $AutoVerifyKey = false ) {
-			$this->ApiKey = $ApiKey;
-			$this->BlogUrl = $BlogUrl;
-			$this->HttpUserAgent = $HttpUserAgent;
+		const PARAM_NAME_BLOG = 'blog';
+		
+		/**
+		* Constructor
+		* 
+		* @param string $ApiKey
+		* @param string $HttpUserAgent
+		* @param string $BlogUrl
+		* @param string $DefaultCommentValues
+		* @param string $AutoVerifyKey
+		* @return AkismetService
+		*/
+		public function __construct( $ApiKey=null, $HttpUserAgent=null, $BlogUrl=null, $DefaultCommentValues=null, $AutoVerifyKey = false ) {
+			$this->setDafaults( $DefaultCommentValues, $BlogUrl, $HttpUserAgent, $ApiKey );
 			$this->AkismetSingleton = AkismetServiceSingleton::getInstance();
 			
 			if( $AutoVerifyKey ) {
@@ -26,7 +40,31 @@
 			}
 		}
 		
+		/**
+		* Set dafult values for all Akismet variables
+		* 
+		* @param array|AkismetComment $CommentValues
+		* @param string $BlogUrl
+		* @param string $HttpUserAgent
+		* @param string $ApiKey
+		*/
+		public function setDafaults( $CommentValues,  $BlogUrl=null, $HttpUserAgent=null, $ApiKey=null ) {
+			if( $CommentValues !== null ) $this->DefaultCommentValues = $CommentValues;
+			if( $BlogUrl !== null ) $this->BlogUrl = $BlogUrl;
+			if( $this->BlogUrl === null ) $this->BlogUrl = $DefaultCommentValues[self::PARAM_NAME_BLOG];
+			
+			if( $HttpUserAgent !== null ) $this->HttpUserAgent = $HttpUserAgent;
+			if( $ApiKey !== null ) $this->ApiKey = $ApiKey;
+		}
+		
 		public function verifyKey( $BlogUrl = null, $HttpUserAgent=null, $ApiKey = null ) {
+            $this->setParamsVars( $BlogUrl, $HttpUserAgent, $ApiKey );
+			$Result = self::verifyKey( $ApiKey, $HttpUserAgent, $BlogUrl );
+			return $Result;
+		}
+
+		protected function setParamsVars( &$BlogUrl, &$HttpUserAgent, &$ApiKey ) {
+			
 			if( $BlogUrl===null ) {
 				$BlogUrl = $this->BlogUrl;
 			}
@@ -38,56 +76,21 @@
 			if( $ApiKey===null ) {
 				$ApiKey = $this->ApiKey;	
 			}
-
-			$Result = self::verifyKey( $ApiKey, $HttpUserAgent, $BlogUrl );
-			return $Result;
 		}
-
+		
 		public function submitSpam( $CommentValues, $BlogUrl=null, $HttpUserAgent=null, $ApiKey=null ) {
-			if( $BlogUrl=== null ) {
-				$BlogUrl = $this->BlogUrl;
-			}
-
-			if( $HttpUserAgent === null ) {
-				$HttpUserAgent = $this->HttpUserAgent;
-			}
-
-			if( $ApiKey === null ) {
-				$ApiKey = $this->ApiKey;
-			}
-			
-			return AkismetServiceSingleton::getInstance()->submitSpam($ApiKey, $HttpUserAgent, $CommentValues, $BlogUrl);
+            $this->setParamsVars( $BlogUrl, $HttpUserAgent, $ApiKey );
+			return $this->AkismetSingleton->submitSpam($ApiKey, $HttpUserAgent, $CommentValues, $BlogUrl);
 		}
 
 		public function submitHam( $CommentValues, $BlogUrl=null, $HttpUserAgent=null, $ApiKey=null ) {
-			if( $BlogUrl=== null ) {
-				$BlogUrl = $this->BlogUrl;
-			}
-
-			if( $HttpUserAgent === null ) {
-				$HttpUserAgent = $this->HttpUserAgent;
-			}
-
-			if( $ApiKey === null ) {
-				$ApiKey = $this->ApiKey;
-			}
-			return self::submitHam( $CommentValues, $BlogUrl, $HttpUserAgent, $ApiKey );
+            $this->setParamsVars( $BlogUrl, $HttpUserAgent, $ApiKey );
+			return $this->AkismetSingleton->submitHam( $CommentValues, $BlogUrl, $HttpUserAgent, $ApiKey );
 		}
 
 		public function checkComment ( $CommentValues, $BlogUrl=null, $HttpUserAgent=null, $ApiKey=null ) {
-			if( $BlogUrl === null) {
-				$BlogUrl = $this->BlogUrl;
-			}
-
-			if( $HttpUserAgent === null ) {
-				$HttpUserAgent = $this->HttpUserAgent;	
-			}		
-
-			if( $ApiKey === null) {
-				$ApiKey = $this->ApiKey;
-			}
-
-			$Result = AkismetServiceSingleton::getInstance()->checkComment( $ApiKey, $HttpUserAgent, $CommentValues, $BlogUrl );
+            $this->setParamsVars( $BlogUrl, $HttpUserAgent, $ApiKey );
+			$Result = $this->AkismetSingleton->checkComment( $ApiKey, $HttpUserAgent, $CommentValues, $BlogUrl );
 			return $Result;
 		}
 	}
