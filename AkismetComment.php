14ad8a696280a82246b0e4f53ba6b3e369fada8a
Valerij Ivashchenko <likemusic@yandex.ru>
2013-06-08 13:04:31 +0300

    add to AkismetComment class creation and setting values from associative array

--- AkismetComment.php
+++ AkismetComment.php
@@ -7,7 +7,7 @@
 	{
 		/**
 		* Constructor
-		* 
+		* @param array $ArrayValues Associative array with values;
 		* @param string $blog The front page or home URL of the instance making the request.
 		* @param string $user_ip IP address of the comment submitter.
 		* @param string $user_agent User agent string of the web browser submitting the comment.
@@ -19,7 +19,7 @@
 		* @param string $comment_author_url URL submitted with comment.
 		* @param string $comment_content The content that was submitted.
 		*/
-		public function __constructor( $blog=null, $user_ip=null, $user_agent=null, $refferer=null, $permalink=null, 
+		public function __constructor( $ArrayValues = null, $blog=null, $user_ip=null, $user_agent=null, $refferer=null, $permalink=null, 
 			$comment_type=null, $comment_author=null, $comment_author_email=null, $comment_author_url=null,
 			$comment_content=null) {
 
@@ -33,6 +33,18 @@
 			$this->comment_author_email = $comment_author_email;
 			$this->comment_author_url = $comment_author_url;
 			$this->comment_content = $comment_content;
+
+			if( $ArrayValues !== null ) {
+				$this->SetFromArray( $ArrayValues );	
+			}
+		}
+
+		public function SetFromArray( $ArrayValues ) {
+			foreach ( get_object_vars($this) as $attr_name => $attr_value ) {
+				if( ( $this->{$attr_name} === null ) && isset( $ArrayValues[$attr_name] ) ) {
+					$this->{$attr_name} = $ArrayValues[$attr_name];
+				}
+			}
 		}
 
 		/**
@@ -44,7 +56,7 @@
 		* @var string
 		*/
 		public $blog;
-		
+
 		/**
 		* IP address of the comment submitter.
 		* (required)
@@ -52,7 +64,7 @@
 		* @var string
 		*/
 		public $user_ip;
-		
+
 		/**
 		* User agent string of the web browser submitting the comment - 
 		* typically the HTTP_USER_AGENT cgi variable.
@@ -62,7 +74,7 @@
 		* @var string
 		*/
 		public $user_agent;
-		
+
 		/**
 		* The content of the HTTP_REFERER header should be sent here.
 		* (note spelling)
@@ -70,42 +82,42 @@
 		* @var string
 		*/
 		public $refferer;
-		
+
 		/**
 		* The permanent location of the entry the comment was submitted to.
 		* 
 		* @var string
 		*/
 		public $permalink;
-		
+
 		/**
 		* May be blank, comment, trackback, pingback, or a made up value like "registration".
 		* 
 		* @var string
 		*/
 		public $comment_type;
-		
+
 		/**
 		* Name submitted with the comment.
 		* 
 		* @var string
 		*/
 		public $comment_author;
-		
+
 		/**
 		* Email address submitted with the comment.
 		* 
 		* @var string
 		*/
 		public $comment_author_email;
-		
+
 		/**
 		* URL submitted with comment.
 		* 
 		* @var string
 		*/
 		public $comment_author_url;
-		
+
 		/**
 		* The content that was submitted.
 		* 
